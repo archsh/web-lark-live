f9c1529f7ef752d54a4c1e002199c4a18b60999e
root <root@hlslivepullout.novalocal>
2017-10-27 07:23:59 +0800

    Updated.

---
+++ raw_data.js
@@ -0,0 +1,33 @@
+﻿/* Data */
+
+var all_places = [
+    {
+        place: 'Default', servers: [
+        {host: '10.112.189.138', port: 5912},
+        {host: '10.112.189.131', port: 5912},
+    ]
+    },
+];
+
+var all_channels = [
+    {
+        source: 'Defaults', channels: [
+        {name: 'CCTV-1', uri: '/lvds/live/cctv-1/live.m3u8'},
+		{name: 'CCTV-2', uri: '/lvds/live/cctv-2/live.m3u8'},
+		{name: 'CCTV-3', uri: '/lvds/live/cctv-3/live.m3u8'},
+		{name: 'CCTV-4', uri: '/lvds/live/cctv-4/live.m3u8'},
+		{name: 'CCTV-5', uri: '/lvds/live/cctv-5/live.m3u8'},
+		{name: 'CCTV-6', uri: '/lvds/live/cctv-6/live.m3u8'},
+		{name: 'CCTV-7', uri: '/lvds/live/cctv-7/live.m3u8'},
+		{name: 'CCTV-8', uri: '/lvds/live/cctv-8/live.m3u8'},
+		{name: 'CCTV-9', uri: '/lvds/live/cctv-9/live.m3u8'},
+		{name: 'CCTV-10', uri: '/lvds/live/cctv-10/live.m3u8'},
+		{name: 'CCTV-11', uri: '/lvds/live/cctv-11/live.m3u8'},
+		{name: 'CCTV-12', uri: '/lvds/live/cctv-12/live.m3u8'},
+		{name: 'CCTV-13', uri: '/lvds/live/cctv-13/live.m3u8'},
+		{name: 'CCTV-14', uri: '/lvds/live/cctv-14/live.m3u8'},
+		{name: 'CCTV-15', uri: '/lvds/live/cctv-15/live.m3u8'},
+		{name: 'CCTV-5+', uri: '/lvds/live/cctv5p/live.m3u8'},
+    ]
+    },
+];
